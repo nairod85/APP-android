981eacbeae0bcd134e43b8c8fe4535d9ed4bf05e
nairod85 <62940082+nairod85@users.noreply.github.com>
2020-05-29 11:50:14 +0200

    First Commit
    First commit

---
+++ app/src/main/java/com/example/projet/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.projet;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ImageButton btnUniv;
+    private ImageButton btnGit;
+    private ImageButton btnMail;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        btnUniv = (ImageButton) findViewById(R.id.btnUniv);
+        btnUniv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.univ-nantes.fr ");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+
+        });
+        btnGit = (ImageButton) findViewById(R.id.btnGit);
+        btnGit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.gitab.univ-nantes.fr ");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+
+        });
+        btnMail = (ImageButton) findViewById(R.id.btnMail);
+        btnMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.webmail-etu.univ-nantes.fr");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate( R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch ( item.getItemId() ) {
+            case R.id.burgerMenu:
+                drawerLayout.openDrawer(Gravity.RIGHT);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    }
